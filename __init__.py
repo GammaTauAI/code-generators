f0f9f6612b928da53e70014916c8a8d75e161e24
cassanof <federico.cassano@federico.codes>
2023-07-13 21:54:24 -0700

    better

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .py_generate import PyGenerator
 from .rs_generate import RsGenerator
+from .lua_generate import LuaGenerator, lua_fix_body
 from .factory import generator_factory, model_factory
 from .model import ModelBase, GPT4, GPT35, StarChat
